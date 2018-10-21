857e874badde8e4d4a01be6c3dbbeec069bff253
Adnan <muh.adnansuryaazis@gmail.com>
2018-10-21 21:51:26 +0800

    install express dan ejs
    run terminal di folder function :
    1. sudo npm install express --save
    2. sudo npm install ejs --save

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + 'public'));
+
+app.get('/', function(request, response)  {
+  response.send("mantap");
+});
+
+
+exports.apps = functions.https.onRequest(app);
