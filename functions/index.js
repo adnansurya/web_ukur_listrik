9c60720cb01cee2cac3930f8c6a23bb7690c1ba3
Adnan <muh.adnansuryaazis@gmail.com>
2018-10-22 01:02:57 +0800

    membuat dynamic title dan appname

--- functions/index.js
+++ functions/index.js
@@ -2,26 +2,27 @@ const functions = require('firebase-functions');
 const express = require('express');
 
 const app = express();
+const appName = "SI ISTRI CERMAT";
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + 'public'));
 
 app.get('/', function(request, response)  {
-  response.render("blank");
+  response.render("beranda", {title : "Beranda", appName : appName});
 });
 app.get('/test', function(request, response)  {
   response.send("ini halaman testing");
 });
 
 app.get('/kosong', function(request, response)  {
-  response.render("blank");
+  response.render("blank", {title : "Blank", appName : appName});
 });
 
 app.get('/status', function(request, response)  {
-  response.render("status");
+  response.render("status", {title : "Status", appName : appName});
 });
 app.get('/login', function(request, response)  {
-  response.render("login");
+  response.render("login", {title : "Login", appName : appName});
 });
 
 exports.apps = functions.https.onRequest(app);
