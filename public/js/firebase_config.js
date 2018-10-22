929ac5990528f114544b5d0bb824cae1468e13d3
Adnan <muh.adnansuryaazis@gmail.com>
2018-10-23 04:49:28 +0800

    delete firebase_config

--- public/js/firebase_config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-   apiKey: "AIzaSyDGud7qFbjXlSh1o_k3wP1A70nNg2BQWmU",
-   authDomain: "si-istri-cermat.firebaseapp.com",
-   databaseURL: "https://si-istri-cermat.firebaseio.com",
-   projectId: "si-istri-cermat",
-   storageBucket: "si-istri-cermat.appspot.com",
-   messagingSenderId: "597083196104"
- };
- firebase.initializeApp(config);
