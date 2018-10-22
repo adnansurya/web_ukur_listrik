68161241ffb11b2e33fde041406f9088c6d4f96d
Adnan <muh.adnansuryaazis@gmail.com>
2018-10-23 04:36:37 +0800

    setup firebase dan vue

---
+++ public/js/firebase_config.js
@@ -0,0 +1,9 @@
+var config = {
+   apiKey: "AIzaSyDGud7qFbjXlSh1o_k3wP1A70nNg2BQWmU",
+   authDomain: "si-istri-cermat.firebaseapp.com",
+   databaseURL: "https://si-istri-cermat.firebaseio.com",
+   projectId: "si-istri-cermat",
+   storageBucket: "si-istri-cermat.appspot.com",
+   messagingSenderId: "597083196104"
+ };
+ firebase.initializeApp(config);
