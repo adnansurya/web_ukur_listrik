0aba069cddcb52ca2954d5438b3a52c96cc30a56
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2019-07-22 16:34:09 +0800

    server http request

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,18 @@
 const functions = require('firebase-functions');
 const express = require('express');
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+
+var admin = require("firebase-admin");
+
+var refreshToken;
+// var serviceAccount = require("/Documents/Project/cromosom/functions/abc.json");
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+
+
+    databaseURL: "https://si-istri-cermat.firebaseio.com/"
+});
+const db = admin.database();
 
 const app = express();
 const appName = "SI ISTRI CERMAT";
@@ -32,6 +43,38 @@ app.get('/log', function(request, response)  {
 });
 app.get('/pengaturan', function(request, response)  {
   response.render("pengaturan", {title : "Pengaturan", appName : appName});
+});
+
+app.get('/update/:watt/:ampere/:delay', function (req, res){
+  let watt, ampere, delay, waktu;
+  watt = req.params.watt;
+  ampere = req.params.ampere;
+  delay = req.params.delay;
+
+  var currentDate = new Date();
+
+  var date = currentDate.getDate();
+  var month = currentDate.getMonth(); //Be careful! January is 0 not 1
+  var year = currentDate.getFullYear();
+  var hour = currentDate.getHours();
+  var minute = currentDate.getMinutes();
+  var second = currentDate.getSeconds();
+
+  var dateString = date + "-" +(month + 1) + "-" + year + " " + hour + ":" + minute +  ":" + second;
+
+  waktu =  dateString;
+  
+
+  db.ref('log/').push({
+
+    waktu: waktu,
+    ampere: ampere,
+    delay : delay,
+    watt : watt
+  });
+
+
+  res.send("Watt : " + watt + " | Ampere : " + ampere + " | Delay :" + delay + " | Waktu : " + waktu );
 });
 
 app.get('/login', function(request, response)  {
